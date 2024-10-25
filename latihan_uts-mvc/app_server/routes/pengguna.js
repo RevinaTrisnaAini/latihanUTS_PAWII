4cad4d2ae1e4939a061f70323ddcd3104d11ed68
Revina Trisna Aini <revinatrisnaaini_2226240102@mhs.mdp.ac.id>
2024-10-25 16:19:36 +0700

    Commit pertama
    membuat project latihan uts , dan memindahkan folder router, view, models ke dalam app_server

---
+++ latihan_uts-mvc/app_server/routes/pengguna.js
@@ -0,0 +1,22 @@
+let mongoose = require("mongoose");
+
+//https://mongoosejs.com/docs/schematypes.html
+//Create Collection Schema
+let schemaPgn = new mongoose.Schema({
+  nama: String,
+  email: {
+    type: String,
+    require: true,
+  },
+   password: {
+    type: String,
+    require: true,
+  },
+  alamat: {
+    type: String,
+    require: true,
+  },
+  tanggal_daftar: {
+    type: Date,
+  },
+});
